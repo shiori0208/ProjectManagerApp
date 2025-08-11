5ecef68c17d6c79a04dfb439894613e0c2f8e1df
shiori0208 <shioriuem02@gmail.com>
2025-08-12 03:38:55 +0530

    Saving current progress and modifications...

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Starting backend ayyy");
+import dotenv from 'dotenv'; //DONOT USE DOUBLE QUOTES FOR PACKAGE NAMES, IT TAKES IT AS STRING!!!
+
+dotenv.config({
+    path: "./.env", //looks for env file in all folders
+});
+
+let myusername = process.env.name;
+//lets you access any data in env files 
+
+console.log("value: ", myusername);
+
+console.log("Starting backend AYYY lessgo");
