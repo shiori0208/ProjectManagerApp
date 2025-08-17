a8c91f2949830801aed93b5211e7aced2f96866c
shiori0208 <shioriuem02@gmail.com>
2025-08-17 21:23:38 +0530

    all setup functions ready to be run...

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,5 +1,44 @@
 import Mailgen from "mailgen"; 
+import nodemailer from "nodemailer"; 
 
+const sendEmail = async (options) => {
+    const mailGenerator = new Mailgen({
+        theme: "default",
+        product: {
+            name: "Project Manager",
+            link: "https://projectmanagelink.com"
+        }
+    })
+
+    const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
+
+    const emailHTML = mailGenerator.generate(options.mailgenContent); 
+
+    const transporter = nodemailer.createTransport({
+        host: process.env.MAILTRAP_SMTP_HOST,
+        port: process.env.MAILTRAP_SMTP_PORT,
+        auth: {
+            user: process.env.MAILTRAP_SMTP_USER,
+            pass: process.env.MAILTRAP_SMTP_PASS
+        }
+
+    })
+
+    const mail = {
+        from: "mail.projectmanager@example.com",
+        to: options.email,
+        subject: options.subject,
+        text: emailTextual,
+        html: emailHTML
+    }
+
+    try {
+        await transporter.sendMail(mail);
+    } catch (error) {
+        console.error("Email service failed. Please make sure you have provided the credentials are correct.");
+        console.error("Error: ", error);
+    }
+}
 
 const emailVerifyMailContent = (username, verificationURL) => {
     return {
@@ -9,12 +48,36 @@ const emailVerifyMailContent = (username, verificationURL) => {
             action: {
                 instructions: "To verify email, please click on the following button",
                 button: {
-                    color: "#f288c4ff",
+                    color: "#f288c4",
                     text: "VERIFY!",
                     link: verificationURL
                 },
             },
+        outro: "Facing issues or need assistance? Contact us or reply to this email! We'll get in touch shortly." 
         },
     };
-
 }
+
+const forgetPassEmailContent = (username, forgetPassURL) => {
+    return {
+        body: {
+            name: username,
+            intro: "Hey, we received a request for reset password of your account.",
+            action: {
+                instructions: "To change password, please click on the following button",
+                button: {
+                    color: "#f2bb88",
+                    text: "RESET PASSWORD!",
+                    link: forgetPassURL
+                },
+            },
+        outro: "Facing issues or need assistance? Contact us or reply to this email! We'll get in touch shortly." 
+        },
+    };
+}
+
+export {
+    emailVerifyMailContent,
+    forgetPassEmailContent,
+    sendEmail
+}; 
