f9cfb8fdb3e3abad03cea0ef6439bff056f7b46c
shiori0208 <68960689+shiori0208@users.noreply.github.com>
2025-08-24 22:30:10 +0530

    mail.js

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -46,7 +46,7 @@ const emailVerifyMailContent = (username, verificationURL) => {
             name: username,
             intro: "Welcome to our project! We are excited to have you onboard!",
             action: {
-                instructions: "To verify email, please click on the following button",
+                instructions: "To verify email, please click on the following button: ",
                 button: {
                     color: "#f288c4",
                     text: "VERIFY!",
